6a98fa7306dc374f5144842ca76005d8e356ec55
Andrea Tribble <atribble@wlapcs.org>
2017-03-02 20:50:18 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+
+}
+
+function draw(){
+    line(35,80,400,200);
+    
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);
+ 
+    line(400,0,400,400);
+    line(400,0,400,400);
+    line(0,200,200,50);
+    line(0,200,600,200);
+    
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(0,10,50,50);
+    
+ 
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(175,350,301,160);
+}
